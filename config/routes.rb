b5b0223c9e6567d54102dceac1b00ce036a0f126
amarkpark <amarkpark@gmail.com>
2015-08-27 00:53:45 +0000

    Generated quotes controller, modified routes.rb, created index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Splurty::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'quotes#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
