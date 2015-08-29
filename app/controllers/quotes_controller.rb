f3a553643d5a8c9fd0ab0ad438a085fdb587b7c2
amarkpark <amarkpark@gmail.com>
2015-08-29 18:51:19 +0000

    Lesson 14: added input validation and error flash. ~AMP

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,7 +8,10 @@ class QuotesController < ApplicationController
 	end
 
 	def create
-		Quote.create(quote_params)
+		@quote = Quote.create(quote_params)
+		if @quote.invalid?
+			flash[:error] = '<strong>Could not add.</strong> The data you entered is invalid.'
+		end
 		redirect_to root_path
 	end
 
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ActiveRecord::Base
+	validates :pickupline, presence: true, length: {minimum: 3, maximum: 120}
+	validates :author, presence: true, length: {minimum: 2, maximum: 100}
 end
