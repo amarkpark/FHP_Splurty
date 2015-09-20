46639c9982652df6a942bdf5d964d6e8297c527e
amarkpark <amarkpark@gmail.com>
2015-09-20 22:26:18 +0000

    TDD3: successfully wrote test, passed test, updated App with unique_tag. ~AMP

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,12 @@
 class Quote < ActiveRecord::Base
 	validates :pickupline, presence: true, length: {minimum: 3, maximum: 120}
 	validates :author, presence: true, length: {minimum: 2, maximum: 100}
+
+	def unique_tag
+		abbr = self.author.split(" ").collect do |namestring|
+			namestring[0]
+		end
+
+		abbr.join + '#' + self.id.to_s
+	end
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
+
+	test "unique_tag" do 
+		quote = Quote.create(:author => 'Keith Earnest', :pickupline => "I love your smile. May I buy you a drink?")
+		expected = "KE#" + quote.id.to_s
+		actual = quote.unique_tag
+		assert_equal expected, actual
+	end
+
 end
