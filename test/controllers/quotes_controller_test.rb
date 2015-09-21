4f480a8898ffb1d81de1b349456a987eb7680af9
amarkpark <amarkpark@gmail.com>
2015-09-21 18:30:36 +0000

    TDD4: finished Show page for Splurty - no link to it though. ~AMP

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "quote show page" do
+    quote = Quote.create(:author => 'Keith Earnest', :pickupline => "I love your smile. May I buy you a drink?")
+	get :show, :id => quote.id
+	assert_response :success
+  end
+
+  test "quote show page, not found" do
+  	get :show, :id => 'OMG'
+  	assert_response :not_found
+  end
+
 end
